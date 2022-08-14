a18bdcc0e08796eb3b4a0053d831a0ce9b7f7902
blessedmadukoma <blessedmadukoma@gmail.com>
2022-08-14 16:55:53 +0100

    built the quicksort algorithm for both golang (used two methods - one using the first element as the pivot and the other using low and high values) and python

--- 4-Quicksort/go/main.go
+++ 4-Quicksort/go/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"my_quicksort/divconq"
+	"my_quicksort/qs_imp"
 )
 
 func main() {
 	// Divide and Conquer implementation
-	list := []int{0, 1, 5, 2, 3, 4, 8}
-	fmt.Println("Total for normal array:", divconq.NormalSum(list))
+	// list := []int{0, 1, 5, 2, 3, 4, 8}
+	// fmt.Println("Total for normal array:", divconq.NormalSum(list))
 
-	list = []int{1, 5, 2, 3, 4, 8}
-	fmt.Println("Total for recursive array:", divconq.RecursiveSum(list))
-	fmt.Println("Total no of elements in recursive array:", divconq.Rec_count_sum(list))
-	fmt.Println("Maximum number recursively:", divconq.RecursiveMax(list))
+	// list = []int{1, 5, 2, 3, 4, 8}
+	// fmt.Println("Total for recursive array:", divconq.RecursiveSum(list))
+	// fmt.Println("Total no of elements in recursive array:", divconq.Rec_count_sum(list))
+	// fmt.Println("Maximum number recursively:", divconq.RecursiveMax(list))
 
 	fmt.Println("-------")
 
 	// Quicksort implementation
-
+	list := []int{1, 10, 5, 2, 3, 4, 8}
+	fmt.Println("Quicksort in action:", qs_imp.QuickSort(list, 0, 6))
+	fmt.Println("Quicksort in action:", qs_imp.QuickSortMine(list))
 }
--- 4-Quicksort/go/qs_imp/quicksort.go
+++ 4-Quicksort/go/qs_imp/quicksort.go
@@ -1,7 +1,58 @@
 package qs_imp
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func QuickSort() {
-	fmt.Println("Quick sort implementation!")
+func QuickSortMine(array []int) []int {
+	start := time.Now()
+	if len(array) < 2 {
+		return array
+	} else {
+		pivot := array[0]
+		less, greater := []int{}, []int{}
+		for _, v := range array[1:] {
+			if v <= pivot {
+				less = append(less, v)
+			} else {
+				greater = append(greater, v)
+			}
+		}
+
+		sortedArray := QuickSortMine(less)
+		sortedArray = append(sortedArray, pivot)
+		for _, v := range QuickSortMine(greater) {
+			sortedArray = append(sortedArray, v)
+		}
+
+		fmt.Println("--Mine:", time.Since(start))
+		return sortedArray
+	}
+}
+
+func QuickSort(arr []int, low, high int) []int {
+	start := time.Now()
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = QuickSort(arr, low, p-1)
+		arr = QuickSort(arr, p+1, high)
+	}
+
+	fmt.Println("--Online:", time.Since(start))
+	return arr
+}
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
 }
