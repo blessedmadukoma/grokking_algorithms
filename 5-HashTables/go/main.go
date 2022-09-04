3f69c916f9ba9b9a36c80fe94efe1c2bfbc401f6
blessedmadukoma <blessedmadukoma@gmail.com>
2022-09-04 14:14:39 +0100

    lesson 5: implementation of hash table as a map in Go

--- 5-HashTables/go/main.go
+++ 5-HashTables/go/main.go
@@ -7,11 +7,17 @@ func main() {
 }
 
 func hashTables() {
-	book := map[string]float32{
-		"apple": 1.04,
-		"milk": 1.49,
-		"avocado": 1.49,
-	}
+	// book := map[string]float32{
+	// 	"apple": 1.04,
+	// 	"milk": 1.49,
+	// 	"avocado": 1.49,
+	// }
+
+	book := make(map[string]float32)
+	book["apple"] = 1.04
+	book["milk"] = 1.49
+	book["avocado"] = 1.49
+
 
 	fmt.Println(book)
 	fmt.Println(book["apple"])
